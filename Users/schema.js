302cf43681207b28de257b84b1d7a336e81d2159
PetoriousBIG <peter.m.martin.1993@gmail.com>
2024-08-02 22:54:09 -0400

    updated user backend

--- Users/schema.js
+++ Users/schema.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
+    username: { type: String},
     password: { type: String, required: true },
     firstName: String,
-    email: String,
+    email: {type: String, required: true, unique: true},
     lastName: String,
     dob: Date,
     role: {
       type: String,
-      enum: ["CRITIC", "CHEF", "OWNER"],
-      default: "CRITIC",
+      enum: ["User", "Chef", "Owner"],
+      default: "User",
     },
     loginId: String,
     lastActivity: Date,
