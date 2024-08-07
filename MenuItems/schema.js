12432bad607de00492b0e59e66da660b32de93fe
PetoriousBIG <peter.m.martin.1993@gmail.com>
2024-08-06 21:19:35 -0400

    Menu Item backend

---
+++ MenuItems/schema.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const menuItemSchema = new mongoose.Schema({
+        restaurant_id: {type: String, required: true},
+        name: {type: String, required: true},
+        description: String,
+        price: Number,
+        chef_id: {type: String, required: true},
+        chef_name: {type: String, required: true},
+        chef_intro: String,
+        images: [String]
+    },
+    { collection: "menu-items" }
+);
+export default menuItemSchema;
